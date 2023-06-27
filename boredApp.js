795be6e93a9c4792847ce80e03819a4a5aa7a72b
AlexDanchenkoIT <125401462+AlexDanchenkoIT@users.noreply.github.com>
2023-06-27 22:44:48 +0300

    init

---
+++ boredApp.js
@@ -0,0 +1,15 @@
+const btnNode = document.querySelector('.js-btn');
+const textNode = document.querySelector('.js-text');
+const titleNode = document.querySelector('.js-title');
+const backGroundNode = document.querySelector('html');
+const notBoredText = 'Ура, теперь не скучно 🔥';
+
+btnNode.addEventListener('click', function(){
+  fetch("http://www.boredapi.com/api/activity/")
+  .then(response => response.json())
+  .then((res) => {
+  textNode.innerText = res.activity;
+  titleNode.innerText = notBoredText;
+  backGroundNode.classList.add('ifGetResponse');
+  })
+})
